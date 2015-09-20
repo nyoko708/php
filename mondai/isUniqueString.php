bbc72ba32af7c80422c38100fd6ed05431edf843
nyoko708 <ckgxd325@yahoo.co.jp>
2015-09-20 22:44:27 +0900

    php mondai 追加

---
+++ mondai/isUniqueString.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 与えた文字列がすべてユニークであるか確認するメソッド
+ *
+ */
+
+$str = "abcdefg";
+
+/**
+ * 文字列が重複していないかどうか？
+ *
+ * @param string $str
+ * @return boolean
+ */
+function isUniqueSting($str)
+{
+
+}
+
+
+
+
---
+++ mondai/reverseString.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 文字列を逆にする関数
+ */
+
+
+/**
+ * 文字を逆順にする
+ *
+ * @param string $str
+ * @return string
+ */
+function reverseString($str)
+{
+}
