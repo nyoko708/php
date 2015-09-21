5fd3f31801d79d5ff9322b9646047afe0d0f76fa
nyoko708 <ckgxd325@yahoo.co.jp>
2015-09-21 23:28:44 +0900

    isUniqueString.php

--- mondai/isUniqueString.php
+++ mondai/isUniqueString.php
@@ -1,10 +1,9 @@
 <?php
 /**
  * 与えた文字列がすべてユニークであるか確認するメソッド
- *
  */
 
-$str = "abcdefg";
+$str = "あいうえおabcdefg";
 
 /**
  * 文字列が重複していないかどうか？
@@ -12,11 +11,27 @@ $str = "abcdefg";
  * @param string $str
  * @return boolean
  */
-function isUniqueSting($str)
+function isUniqueString($str)
 {
+  $count = mb_strlen(trim($str), "UTF-8");
 
+  $strArray = [];
+  for($i=0; $i<$count; $i++) {
+    $strArray[] = mb_substr($str, $i, 1, "UTF-8");
+  }
+
+  $uniqueFlags = array();
+  for($i=0; $i<$count; $i++) {
+    $val = $strArray[$i];
+    if(array_key_exists($val, $uniqueFlags) && $uniqueFlags[$val] === true) {
+      return false;
+    }
+    $uniqueFlags[$val] = true;
+  }
+
+  return true;
 }
 
 
-
+var_dump(isUniqueString($str));
 
