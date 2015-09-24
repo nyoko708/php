5fd1df305bc7b3a068305bb4a9c837bf9511154d
nyoko708 <ckgxd325@yahoo.co.jp>
2015-09-24 21:32:43 +0900

    edit reverseString

--- mondai/reverseString.php
+++ mondai/reverseString.php
@@ -14,14 +14,9 @@ function reverseString($str)
 {
   $length = mb_strlen($str, "UTF-8");
 
-  $strs = array();
-  for($i=0; $i<$length; $i++) {
-    $strs[] = mb_substr($str, $i, 1, "UTF-8");
-  }
-
   $reverse = "";
   for($i=$length-1; $i>=0; $i--) {
-    $reverse .= $strs[$i];
+    $reverse .= mb_substr($str, $i, 1, "UTF-8");
   }
 
   return $reverse;
