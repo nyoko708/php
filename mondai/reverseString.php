cbf37c9f80607aa4f1e24038e3e0a121b4769431
nyoko708 <ckgxd325@yahoo.co.jp>
2015-09-24 21:10:02 +0900

    reverse

--- mondai/reverseString.php
+++ mondai/reverseString.php
@@ -12,4 +12,21 @@
  */
 function reverseString($str)
 {
+  $length = mb_strlen($str, "UTF-8");
+
+  $strs = array();
+  for($i=0; $i<$length; $i++) {
+    $strs[] = mb_substr($str, $i, 1, "UTF-8");
+  }
+
+  $reverse = "";
+  for($i=$length-1; $i>=0; $i--) {
+    $reverse .= $strs[$i];
+  }
+
+  return $reverse;
 }
+
+
+$str = "あい　うえお abcd efg";
+var_dump(reverseString($str));
